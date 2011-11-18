c951f92d2c7e36b90dac9d3bd98c4d2403f66dfb
Timothy Palpant <tim@palpant.us>
2011-11-18 13:33:37 -0500

    Add autocorrelation script

---
+++ nucleosome/autocorrelation.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/env ruby1.9
+
+# == Synopsis 
+#   Compute the autocorrelation of a wig dataset for a list of windows
+#
+# == Usage 
+#   Take sequencing data in nukes.wig and compute the autocorrelation on
+#   the list of windows specified in orfs.bed
+#
+#   powerSpectrum.rb -i nukes.wig -l orfs.bed -o fft.txt
+#
+#   For help use: powerSpectrum.rb -h
+#
+# == Options
+#   -h, --help          Displays help message
+#   -i, --input         Input file with nucleosome sequencing data
+#   -l, --loci          Windows to compute the autocorrelation on
+#   -o, --output        Output file with autocorrelation values
+#
+# == Author
+#   Timothy Palpant
+#
+# == Copyright
+#   Copyright (c) 2011 Timothy Palpant. Licensed under the GPL v3:
+#   http://www.gnu.org/licenses/gpl-3.0.txt
+
+COMMON_DIR = File.expand_path(File.dirname(__FILE__) + '/../common')
+$LOAD_PATH << COMMON_DIR unless $LOAD_PATH.include?(COMMON_DIR)
+require 'bundler/setup'
+require 'pickled_optparse'
+require 'bio-genomic-file'
+require 'fftw3'
+include Bio
+
+# This hash will hold all of the options parsed from the command-line by OptionParser.
+options = Hash.new
+ARGV.options do |opts|
+  opts.banner = "Usage: ruby #{__FILE__} -i reads.sam -o dyads.wig"
+  # This displays the help screen, all programs are assumed to have this option.
+  opts.on( '-h', '--help', 'Display this screen' ) do
+    puts opts
+    exit
+  end
+  
+  # Input/output arguments
+  opts.on( '-i', '--input FILE', :required, "Input file (wig)" ) { |f| options[:input] = f }
+  opts.on( '-l', '--loci FILE', :required, "Windows to compute the DFT on (bed)" ) { |name| options[:loci] = name }
+  opts.on( '-o', '--output FILE', :required, "Output file (bedgraph-ish)" ) { |f| options[:output] = f }
+  
+  # Parse the command-line arguments
+  opts.parse!
+  
+  # Validate the required parameters
+  if opts.missing_switches?
+    puts opts.missing_switches
+    puts opts
+    exit
+  end
+end
+
+
+# Initialize the sequencing data
+wig = WigFile.autodetect(options[:input])
+
+# Iterate over the list of windows
+# and compute the power spectrum for each
+File.open(options[:output], 'w') do |f|
+  f.puts "ORF\tChromosome\tStart (+1 Nuc)\tStop (3' Nuc)\tL\tAutocorrelation"
+
+  BedFile.foreach(options[:loci]) do |spot|
+    next if spot.length <= 1
+    
+    # Get the data from the Wig
+    begin
+      data = NArray.to_na(wig.query(spot.chr, spot.low, spot.high).to_a)
+    rescue WigError
+      next
+    end
+    
+    # Compute the autocorrelation, using the Wiener–Khinchin theorem
+    autocorrelation = FFTW3.ifft(FFTW3.fft(data).abs**2)
+    
+    f.puts "#{spot.id}\t#{spot.chr}\t#{spot.start}\t#{spot.stop}\t#{(spot.start-spot.stop).abs}\t#{autocorrelation.join("\t")}"
+  end
+end
